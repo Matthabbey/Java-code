ae4bb8f3d888932af230e8feff9c37093e07a1de
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-03-09 23:39:08 +0100

    still building

--- src/starterbeginner/Main.java
+++ src/starterbeginner/Main.java
@@ -3,6 +3,10 @@ package starterbeginner;
 public class Main {
     public static void main(String[] args) {
         int age = 50;
+        long num = 3_123_456_789L;
+        char isAlpha = 'A';
+        float allAph = 10.99F;
+        boolean isWorking = false;
         int herAge = age;
         System.out.println(age);
         System.out.println("Hello World!");
