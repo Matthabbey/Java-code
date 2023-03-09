ccd26b6600016ba2fb51c7918901236be0250eb2
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-03-09 22:29:16 +0100

    first commit

---
+++ src/starterbeginner/Main.java
@@ -0,0 +1,10 @@
+package starterbeginner;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 50;
+        int herAge = age;
+        System.out.println(age);
+        System.out.println("Hello World!");
+    }
+}
