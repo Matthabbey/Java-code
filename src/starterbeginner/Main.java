ef4118eb0ab519986b86a0d26ef4dc614563edab
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-03-12 06:34:12 +0100

    ControFlow

--- src/starterbeginner/Main.java
+++ src/starterbeginner/Main.java
@@ -153,3 +153,4 @@ public class Main {
         System.out.print("Mortgage: " + NumberFormat.getCurrencyInstance().format(output));
     }
 }
+
---
+++ src/starterbeginner/controlFlow.java
@@ -0,0 +1,12 @@
+package starterbeginner;
+
+class controlFlow {
+    public static void main(String[] args) {
+        int temp = 30;
+        if(temp > 20){
+            System.out.println(false);
+        }else if(temp == 30){
+            System.out.println(true);
+        }
+    }
+}
